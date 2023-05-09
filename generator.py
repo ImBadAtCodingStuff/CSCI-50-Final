30789c704fecf15dfb53342d09ef049b578baba4
Evan White <teslafan3261@gmailcom>
2023-05-09 10:21:46 -0700

    Added ability to save to the database in the generator script

--- generator.py
+++ generator.py
@@ -1,5 +1,10 @@
 # This is a script to generate a password for the user to use for one of there accounts
 import random
+import sqlite3
+
+# setup for connecting to the databse
+databseconnection=sqlite3.connect("database.db")
+cursor=databseconnection.cursor()
 
 # chars
 chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -13,13 +18,26 @@ chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 # add it to a string
 # display the password
 
+def save_to_database(username, password):
+    cursor.execute(f"""INSERT INTO accounts VALUES ({username}, {password})""")
+    databseconnection.commit()
+    if (cursor.execute(f"""SELECT * FROM account WHERE username={username}""") is not None):
+        print("Account data saved sucessfully...")
 
-length = int(input("How long would you like the password: "))
+
 
 def generate(length):
     password = ""
     for i in range(length):
         password+=random.choice(chars)
-    print(password)
+    return password
 
-generate(length)
+def main():
+    while 1:
+        password=generate(int(input("Password length: ")))
+        if (input("Do you like: "+password+" as your password: (y/n) ")=="y"):
+            save_to_database(input("Enter the username for the account: "), password)
+        else:
+            print("Lets try another one")
+
+main()
