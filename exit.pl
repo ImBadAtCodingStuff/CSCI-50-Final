2a0622427ebc480fd4115da0c1dc38759668d816
nickathryne <48330412+nickathryne@users.noreply.github.com>
2023-05-09 12:53:17 -0700

    Add files via upload

---
+++ exit.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#	Primary Author: Nicole Niesen
+
+print ("Would you like to exit the program? Y/N: ");
+$choice = <STDIN>;
+chomp($choice);
+$ch = lc $choice;
+$yes = "y";
+$no = "n";
+if ($ch eq $yes) {
+	exit;
+}
