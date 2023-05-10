762a181aa9be7624bac96786820374359d66d753
nickathryne <48330412+nickathryne@users.noreply.github.com>
2023-05-10 15:38:36 -0700

    Add files via upload
    fixed table insert

--- generator.py
+++ generator.py
@@ -20,10 +20,10 @@ chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 # add it to a string
 # display the password
 
-def save_to_database(username, password):
-    cursor.execute(f"""INSERT INTO Password_Data VALUES ('{username}', '{password}')""")
+def save_to_database(service, username, password):
+    cursor.execute(f"""INSERT INTO Password_Data VALUES ('{service}', '{username}', '{password}')""")
     databseconnection.commit()
-    if (cursor.execute(f"""SELECT * FROM account WHERE username='{username}'""") is not None):
+    if (cursor.execute(f"""SELECT * FROM password_data WHERE username='{username}'""") is not None):
         print("Account data saved sucessfully...")
 
 
@@ -38,7 +38,9 @@ def main():
     while 1:
         password=generate(int(input("Password length: ")))
         if (input("Do you like: "+password+" as your password: (y/n) ")=="y"):
-            save_to_database(input("Enter the username for the account: "), password)
+            service = input("Which service would you like to save this password under? ")
+            save_to_database(service, input("Enter the username for the account: "), password)
+            break
         else:
             print("Lets try another one")
 
