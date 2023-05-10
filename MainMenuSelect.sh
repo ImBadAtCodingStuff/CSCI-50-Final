cc97b01cced522f39e7f612b27b9b974bf4fbafa
Emma4112 <129696368+Emma4112@users.noreply.github.com>
2023-05-09 21:29:39 -0700

    Update MainMenuSelect.sh

--- MainMenuSelect.sh
+++ MainMenuSelect.sh
@@ -7,19 +7,25 @@ while true; do
   do
     if [ $Reply == "Add_Record" ]; then
       python3 addRecord.py
+      break
     elif [ $Reply == "Search_Records" ]; then
       chmod u+rx SearchRecords.sh
       ./SearchRecords.sh
+      break
     elif [ $Reply == "Edit_Record" ]; then
       python3 editRecord.py
+      break
     elif [ $Reply == "Delete_Record" ]; then
       chmod u+rx DeleteRecord.sh
       ./DeleteRecord.sh
+      break
     elif [ $Reply == "Password_Generator" ]; then
       python3 generator.py
+      break
     elif [ $Reply == "Exit" ]; then
       chmod u+rx exit.pl
       ./exit.pl
+      break
       exit
     else 
       echo "Invalid input."
