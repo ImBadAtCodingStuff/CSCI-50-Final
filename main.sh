21d2f0f1199960eeee2ce1c21333d41ee8a64476
nickathryne <48330412+nickathryne@users.noreply.github.com>
2023-05-09 20:04:31 -0700

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+chmod +x DeleteRecord.sh
+chmod +x MainMenuSelect.sh
+chmod +x SearchRecord.sh
+chmod +x addRecord.py
+chmod +x edit.py
+chmod +x editRecord.py
+chmod +x encryption.py
+chmod +x exit.pl
+chmod +x generator.py
+chmod +x start_config.py
+
+python3 start_config.py
