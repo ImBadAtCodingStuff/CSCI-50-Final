2bcf5a268b2ff547ea847f12742ebef20524a29d
Emma4112 <129696368+Emma4112@users.noreply.github.com>
2023-05-11 17:52:28 -0700

    Update DeleteRecord.sh
    added awk and some regex

--- DeleteRecord.sh
+++ DeleteRecord.sh
@@ -4,7 +4,7 @@
 DeleteConfirm () {
   delvar="$1"
   echo "Do you want to delete this record? This cannot be undone."
-  while true; do 
+  while true; do
     echo "Enter Y to delete record."
     echo "Enter N to keep record."
     read -p "Enter selection: " delx
@@ -28,19 +28,43 @@ status="y"
 while [ $status = "y" ]; do
         echo "Password Squirrel"
         echo "Delete Record"
-        read -p "Enter name of service to delete record for: " delrec
-        count=$(sqlite3 PasswordSquirrel.db "SELECT COUNT(*) FROM Password_Data WHERE Service='$delrec' COLLATE NOCASE") 
+        read -p "Enter name of service/username to delete record for: " delrec
+        count=$(sqlite3 PasswordSquirrel.db "SELECT COUNT(*) FROM Password_Data WHERE Service LIKE '%$delrec%' COLLATE NOCASE OR Username LIKE '%$delrec%' COLLATE NOCASE")
         if [ $count -eq 0 ]; then
           echo
           echo "No matching records found."
           sleep 2
         else
           echo
-          echo "Matching record found:"
-          echo "Service|Username|Password"
-          sqlite3 PasswordSquirrel.db "SELECT * FROM Password_Data WHERE Service='$delrec' COLLATE NOCASE"
+          echo "Matching record(s) found:"
+          sqlite3 PasswordSquirrel.db "SELECT rowid, * FROM Password_Data WHERE Service LIKE '%$delrec%' COLLATE NOCASE OR Username LIKE '%$delrec%' COLLATE NOCASE" | awk -F"|" '{printf "Record ID: %s\nService: %s\nUsername: %s\nPassword: %s\n\n", $1, $2, $3, $4}'
           echo
-          DeleteConfirm "$delrec"    
+          if [ $count -eq 1 ]; then
+                read -p "Enter the Record ID of the record you want to delete: " delid
+                delcheck=(sqlite3 PasswordSquirrel.db "SELECT Service FROM Password_Data WHERE rowid=$delid")
+                if [ "$delcheck" ]; then
+                        delrec=$(sqlite PasswordSquirrel.db "SELECT Service FROM Password_Data WHERE rowid=$delid")
+                        DeleteConfirm "$delrec"
+                else
+                        echo "Invalid choice. Try again."
+                fi
+          else
+                while true; do
+                        read -p "Enter the Record ID of the record you want to delete: " delid
+                        if [ -z "$delid" ]; then
+                                echo "Invalid choice. Try again."
+                        else
+                                delcheck=$(sqlite3 PasswordSquirrel.db "SELECT Service FROM Password_Data WHERE rowid=$delid")
+                                if [ "$delcheck" ]; then
+                                        delrec=$(sqlite3 PasswordSquirrel.db "SELECT Service FROM Password_Data WHERE rowid=$delid")
+                                        DeleteConfirm "$delrec"
+                                        break
+                                else
+                                        echo "Invalid choice. Try again."
+                                fi
+                        fi
+                done
+          fi
         fi
         echo
         echo "Enter Y to delete another record."
@@ -59,3 +83,5 @@ while [ $status = "y" ]; do
             fi
         done
 done
+
+
